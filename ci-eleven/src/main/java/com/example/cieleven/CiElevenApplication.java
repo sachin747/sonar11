28c422a450325e3fff6ef78d6ba00051ccd50984
sachin747 <54313930+sachin747@users.noreply.github.com>
2021-09-13 13:16:47 +0530

    Add files via upload
    inital code

---
+++ ci-eleven/src/main/java/com/example/cieleven/CiElevenApplication.java
@@ -0,0 +1,13 @@
+package com.example.cieleven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiElevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiElevenApplication.class, args);
+	}
+
+}
---
+++ ci-eleven/src/test/java/com/example/cieleven/CiElevenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cieleven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiElevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
